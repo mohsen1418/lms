887fba5408571fa3c7ab3b6c865e824eef3ccab6
mohsen1418 <mohsen1418.mm@gmail.com>
2022-09-10 13:03:30 +0430

    update

--- resources/views/errors/404.blade.php
+++
@@ -1,5 +0,0 @@
-@extends('errors::minimal')
-
-@section('title', __('متاسفیم'))
-@section('code', 'متاسفانه درخواست شما یافت نشد')
-@section('message', __('پیام سیستم'))
--- resources/views/message/charge.blade.php
+++
@@ -1,4 +0,0 @@
-<?php include('jdf.php') ?>
-@extends('layout.app')
-@section('content')
-@endsection
